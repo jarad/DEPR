d7f547c85f9967b002dae0d7a283d7d63e89b07f
Jarad Niemi <niemi@iastate.edu>
2023-05-11 16:36:53 -0500

    Add env file

--- tests/testthat/test-read_env.R
+++ tests/testthat/test-read_env.R
@@ -1,4 +1,4 @@
-env_file <- system.file("extdata", "071000090603_2.env", package="WEPPR")
+env_file <- system.file("extdata", "071000090603_2.env", package="DEPR")
 
 test_that("no error", {
     expect_error(read_env(env_file, start.year=2007), NA)
